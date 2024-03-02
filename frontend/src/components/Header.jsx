ee86f954d9e048a0c37c4e164654fbf1964d2918
Josimar <josimarvictoria968@gmail.com>
2024-03-02 12:07:46 -0500

    update header component with user data

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,16 @@
-import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
-import { Link, useLocation } from "react-router-dom";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
-import { useState } from "react";
+import { useSelector } from 'react-redux';
+import {  useState } from 'react';
 
 export default function Header() {
-    const [theme, setHeme] = useState(true)
+  const [theme, setHeme] = useState(true);
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
+
+
   return (
     <Navbar className="border-b-2">
       <Link
@@ -38,35 +42,35 @@ export default function Header() {
           pill
           onClick={() => {}}
         >
-           
-          {theme === 'light' ? <FaSun /> : <FaMoon />}
+          {theme === "light" ? <FaSun /> : <FaMoon />}
         </Button>
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar alt="user" img='https://github.com/victoria.png' rounded />
-          }
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">@josim</span>
-            <span className="block text-sm font-medium truncate">
-              josima@gmail.com
-            </span>
-          </Dropdown.Header>
-          <Link to={"/dashboard?tab=profile"}>
-            <Dropdown.Item>Profile</Dropdown.Item>
-          </Link>
-          <Dropdown.Divider />
-          <Dropdown.Item onClick="">Sign out</Dropdown.Item>
-        </Dropdown>
-
-        <Link to='/sign-in'>
-            <Button gradientDuoTone='purpleToBlue' outline>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick="">Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
               Sign In
             </Button>
           </Link>
-
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
