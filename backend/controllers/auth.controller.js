2537a1275fe924632246644ce6ac8fa9568fd44b
Josimar <josimarvictoria968@gmail.com>
2024-03-02 03:19:01 -0500

    creando signin api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHendler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 // Controlador para el registro de usuarios
 export const singupAuthController = async (req, res, next) => {
@@ -10,7 +11,14 @@ export const singupAuthController = async (req, res, next) => {
 
   try {
     // Verificar campos requeridos
-    if (!username || !email || !password || username === "" || email === "" || password === "") {
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
       next(errorHendler(400, "All fields are required"));
     }
 
@@ -32,6 +40,51 @@ export const singupAuthController = async (req, res, next) => {
   } catch (error) {
     // Manejo de errores
     console.log(error);
+    next(error);
+  }
+};
+
+// Controlador para el inicio de sesión de usuarios
+export const signinAuthController = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Verificar campos requeridos
+  if (!email || !password || email === "" || password === "") {
+    next(errorHendler(400, "All fields are required"));
+  }
+
+  try {
+    // Buscar al usuario en la base de datos
+    const validUser = await User.findOne({ email });
+
+    // Si el usuario no se encuentra, devolver un error
+    if (!validUser) {
+      return next(errorHendler(404, "User not found"));
+    }
+
+    // Validar la contraseña utilizando bcrypt
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+
+    // Si la contraseña no es válida, devolver un error
+    if (!validPassword) {
+      return next(errorHendler(400, "Invalid password"));
+    }
+
+    // Generar un token JWT
+    const token = jwt.sign({ id: validUser._id }, "HgKiasklqi7KAQI1hoq1");
+
+    // Extraer la contraseña del objeto de usuario y enviar el resto de los datos
+    const { password: pass, ...rest } = validUser._doc;
+
+    // Enviar una respuesta exitosa con el token y los datos del usuario
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    // Manejo de errores
     next(error);
   }
 };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,13 @@
 // Importar módulos necesarios
 import express from "express";
-import { singupAuthController } from "../controllers/auth.controller.js";
+import { signinAuthController, singupAuthController } from "../controllers/auth.controller.js";
 
 // Crear un enrutador para manejar las rutas
 const router = express.Router();
 
 // Definir la ruta para el registro de usuarios
 router.post("/signup", singupAuthController);
+router.post('/signin', signinAuthController);
 
 // Exportar el enrutador para su uso en otras partes de la aplicación
 export default router;
