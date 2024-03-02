5091eeebaa6867b971fd78144bdcd6d589c8ab6d
Josimar <josimarvictoria968@gmail.com>
2024-03-01 22:43:13 -0500

    connect to the database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config()
 
+// process.env.MONGODB_CONNECT
+mongoose
+  .connect(
+    "mongodb+srv://josimarvictoria968:TIKnRpZjPb5c0qhV@dashboard-mern.usnbpo9.mongodb.net/dashboard-mern?retryWrites=true&w=majority&appName=dashboard-mern"
+  )
+  .then(() => {
+    console.log("Mongoose is connected");
+  }).catch(err => {
+    console.log(err);
+  });
+
+const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is running on port http://localhost/3000")
-})
+  console.log("Server is running on port http://localhost/3000");
+});
